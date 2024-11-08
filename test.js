ad6feee30edcd7e03b2b37f1ff2d1a042d1ba488
Navneet Gond <146729662+nvnt98@users.noreply.github.com>
2024-11-08 15:20:24 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+document.getElementById("sub").addEventListener("click", function(){
+    var in1= document.getElementById("i1");
+    var in2= document.getElementById("i2");
+    console.log(document.getElementById("i1"));
+    
+})
+
